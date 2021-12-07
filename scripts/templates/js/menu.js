2f7f252bfba24a35e87e3eb18fa40deecb60bf70
Antonio Garrote <antoniogarrote@gmail.com>
2021-12-07 02:13:11 -0800

    CIM 1.0.0

--- scripts/templates/js/menu.js
+++
@@ -1,5 +0,0 @@
-function menuToggle(button) {
-  let menu = document.getElementById("aside");
-  menu.classList.toggle("open");
-  button.innerHTML === "Menu &gt;" ? button.innerHTML = "&lt; Close" : button.innerHTML = "Menu &gt;";
-}
